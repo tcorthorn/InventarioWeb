93437cfda3a53e533c57783b23489bfab60b7fa9
Thomas <t.corthorn@gmail.com>
2022-05-29 14:57:42 -0400

    inicio

---
+++ inventario/urls.py
@@ -0,0 +1,18 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('oficina/', include('oficina.urls')),
+    path('', RedirectView.as_view(url='/oficina/', permanent=True)),
+]
+
+# Use static() to add url mapping to serve static files during development (only)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
