91277255cde5cd2842dc6d122ee55917b2a8bf0c
Thomas <t.corthorn@gmail.com>
2022-06-02 18:53:27 -0400

    primeras tablas

--- oficina/admin.py
+++ oficina/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Proveedor, Destino, Ingreso, Salida
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from datetime import date
 
-# Register your models here.
+class InventResource(resources.ModelResource):
+    class Meta:
+        model = Proveedor
+        model = Destino
+        model = Ingreso
+        model = Salida
+     
+@admin.register(Proveedor)
+class ProveedorAdmin(ImportExportModelAdmin,admin.ModelAdmin):
+    list_display = ('nombre',)
+    list_filter = ('nombre',)
+    resources_class = Proveedor
+    search_fields = ("nombre", )
+    
+@admin.register(Destino)
+class DestinoAdmin(ImportExportModelAdmin,admin.ModelAdmin):
+    list_display = ('nombre',)
+    list_filter = ('nombre',)
+    resources_class = Destino
+    search_fields = ("nombre", )
+
+@admin.register(Ingreso)
+class IngresoAdmin(ImportExportModelAdmin,admin.ModelAdmin):
+    list_display = ('fecha','sku','cantidad', 'comentario')
+    list_filter = ('sku', 'fecha')
+    resources_class = Ingreso
+    search_fields = ("sku", )
+
+@admin.register(Salida)
+class SalidaAdmin(ImportExportModelAdmin,admin.ModelAdmin):
+    list_display = ('fecha','sku','cantidad', 'display_destino', 'comentario')
+    list_filter = ('sku', 'fecha')
+    resources_class = Salida
+    search_fields = ("sku", )
--- oficina/models.py
+++ oficina/models.py
@@ -1,2 +1,86 @@
 from django.db import models
+from django.forms import CharField, IntegerField
+from django.urls import reverse
+from datetime import date
 
+class Proveedor(models.Model):
+    nombre = models.CharField(max_length=10, default='Otro',null=True,blank=True)
+    
+    def __str__(self):
+        return self.nombre
+
+    def get_absolute_url(self):
+        """
+        Retorna la url para acceder a una instancia particular de un proveedor.
+        """
+        return reverse('detalle-proveedor', args=[str(self.id)])
+
+    class Meta():
+        verbose_name='proveedor'
+        verbose_name_plural ='Proveedores'
+
+    class Meta():
+        ordering =['nombre']
+
+class Destino(models.Model):
+    nombre = models.CharField(max_length=100, default='Otro', help_text='Ingrese destino del despacho')
+    
+    def __str__(self):
+        return self.nombre
+
+    def get_absolute_url(self):
+        """
+        Retorna la url para acceder a una instancia particular de un proveedor.
+        """
+        return reverse('detalle-destino', args=[str(self.id)])
+
+    class Meta():
+        ordering =['nombre']
+
+class Ingreso(models.Model):
+    fecha = models.DateField()
+    sku = models.CharField(max_length=7)
+    cantidad = models.IntegerField()
+    proveedor = models.ManyToManyField(Proveedor) 
+    comentario = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.sku
+
+    def get_absolute_url(self):
+        """
+        Retorna la url para acceder a una instancia particular de un proveedor.
+        """
+        return reverse('detalle-ingreso', args=[str(self.id)])
+
+    class Meta():
+        ordering =['-fecha']
+
+class Salida(models.Model):
+    fecha = models.DateField()
+    sku = models.CharField(max_length=7)
+    cantidad = models.IntegerField()
+    destino = models.ManyToManyField(Destino) 
+    comentario = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.sku
+
+    def get_absolute_url(self):
+        """
+        Retorna la url para acceder a una instancia particular
+        """
+        return reverse('detalle-destino', args=[str(self.id)])
+
+    def display_destino(self):
+
+        """"         Creates a string for the cliente. This is required to display destino in Admin.        """
+
+        return ', '.join([ destino.nombre for destino in self.destino.all()[:3] ])
+        display_destino.short_description = 'Destino'
+
+    class Meta():
+        ordering =['-fecha']
+
+
+    
