fa18692de3d63d22ead29da18682a4882e94d870
Thomas <t.corthorn@gmail.com>
2022-06-22 19:19:07 -0400

    base de datos

--- inventario/settings/production.py
+++ inventario/settings/production.py
@@ -13,10 +13,10 @@ ALLOWED_HOSTS = ['inventarioboran.herokuapp.com']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'InventarioWeb1',
-        'USER': 'postgres',
-        'PASSWORD': '123321',
-        'HOST': '127.0.0.1',
+        'NAME': 'd5601d5b9m2k2i',
+        'USER': 'bmeijvomkhmmhg',
+        'PASSWORD': '146ffe81e12d43b90c6babe04422bd3a77a86e1836041ef1751a7f800772adec',
+        'HOST': 'ec2-52-72-99-110.compute-1.amazonaws.com',
         'DATABASE_PORT':'5432',
     }
 }
